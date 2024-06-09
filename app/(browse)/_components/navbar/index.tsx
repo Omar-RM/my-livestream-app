8c0fb60eddde09349dddc636325b9b0b0df39f8f
Red-Dragn <omarm.rico.98@gmail.com>
2024-06-09 20:36:35 +0100

    Search section

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,8 +1,10 @@
 import { Logo } from "./logo";
+import { Search } from "./Search";
 export const NavBar = () => {
   return (
     <nav className="fixed top-0 w-full h-20 z-{49} bg-[#252731] px-2 lg:px4 flex justify-between items-center shadow-sm">
-      <Logo></Logo>
+      <Logo />
+      <Search />
     </nav>
   );
 };
