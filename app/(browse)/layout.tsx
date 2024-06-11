ee2bc1f1968ef0d62d4a2a3cc6a5b737de609ceb
Red-Dragn <omarm.rico.98@gmail.com>
2024-06-11 23:49:05 +0100

    create sidebar.tsx

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,9 +1,14 @@
+import { Sidebar } from "lucide-react";
 import { NavBar } from "./_components/navbar";
+import { SideBar } from "./_components/sidebar";
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NavBar />
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <SideBar />
+        {children}
+      </div>
     </>
   );
 };
