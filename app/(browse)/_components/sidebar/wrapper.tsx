051093dddbac5f934c001910d269db50518216a7
Red-Dragn <omarm.rico.98@gmail.com>
2024-06-25 22:22:57 +0100

    fixing loading error in use client

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,15 +2,25 @@
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 
+import { useState, useEffect } from "react";
+
 interface WrapperProps {
   children: React.ReactNode;
 }
 export const Wrapper = ({ children }: WrapperProps) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   const { collapsed } = useSidebar((state) => state);
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r boder-[#2D2E35] z-50",
+        "fixed left-0 flex flex-col lg:w-60 h-full bg-background border-r boder-[#2D2E35] z-50",
         collapsed && "w-[70px]"
       )}
     >
